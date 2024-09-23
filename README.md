50aee0d7854a58961fbf0c55d58a13e6ae862c21
Máté Brösztl <brosztl.mate@gmail.com>
2024-09-23 09:14:10 +0200

    Implement Brainwave: Advanced UI/UX Framework for Modern Web

---
+++ README.md
@@ -0,0 +1,48 @@
+# Brainwave: Modern UI/UX website
+
+## 🤖 Introduction
+
+Develop a cutting-edge website with Brainwave, an advanced UI/UX framework built on React.js and Tailwind CSS, showcasing modern design principles.
+
+Empower developers to create visually stunning and highly responsive web applications, featuring sleek interfaces, smooth animations, and exceptional user experiences. Set new standards in web design with this innovative tool, perfect for inspiring and guiding the creation of next-generation websites and applications.
+
+## ⚙️ Tech Stack
+
+- React
+- Javascript
+- Tailwind CSS
+
+## 🚀 Quick Start
+
+Follow these steps to set up the project locally on your machine.
+
+**Prerequisites**
+
+Make sure you have the following installed on your machine:
+
+- Git
+- Node.js
+- npm (Node Package Manager)
+
+**Cloning the Repository**
+
+```bash
+git clone https://github.com/mate10106/brainwave.git
+cd brainwave
+```
+
+**Installation**
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+
+**Running the Project**
+
+```bash
+npm run dev
+```
+
+Open **http://localhost:5173** in your browser to view the project.
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,39 @@
+import Section from "./Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
+
+const Pricing = () => {
+  return (
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.4rem] lg:flex">
+          <img src={smallSphere} height={255} width={255} alt="Sphere" />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              className="w-full"
+              width={950}
+              height={400}
+              alt="stars"
+            />
+          </div>
+        </div>
+
+        <Heading
+          tag="get started with Brainwave"
+          title="Pay once, use forever"
+        />
+
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Pricing;
